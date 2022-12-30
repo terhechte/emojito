344f9318b3b46e564fba89f837a8b0761fbf1b9d
Benedikt Terhechte <terhechte@gmail.com>
2022-12-30 16:38:23 +0100

    speed improvements and emoji improvements by switching to icu and emojis

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@
 //! ```
 use std::ops::Range;
 
-use emoji::lookup_by_glyph;
-
-pub use emoji::Emoji;
+use emojis::get as lookup_emoji;
+pub use emojis::Emoji;
+use icu_properties::sets;
 
 /// Find all the emoji in a string. Returns the emoji in a `Vec`.
 /// ``` rs
@@ -44,12 +44,25 @@ fn find_emoji_ranges(
     let mut emoji_list = Vec::with_capacity(128);
     let mut container = String::with_capacity(8);
 
+    let is_emoji_data = sets::load_emoji(&icu_testdata::unstable()).unwrap();
+    let is_emoji_ref = is_emoji_data.as_borrowed();
+    let is_emoji_component_data = sets::load_emoji_component(&icu_testdata::unstable()).unwrap();
+    let is_emoji_component_ref = is_emoji_component_data.as_borrowed();
+    let is_emoji_modifier_data = sets::load_emoji_modifier(&icu_testdata::unstable()).unwrap();
+    let is_emoji_modifier_ref = is_emoji_modifier_data.as_borrowed();
+    let is_emoji_modifier_base_data =
+        sets::load_emoji_modifier_base(&icu_testdata::unstable()).unwrap();
+    let is_emoji_modifier_base_ref = is_emoji_modifier_base_data.as_borrowed();
+    let is_emoji_presentation_data =
+        sets::load_emoji_presentation(&icu_testdata::unstable()).unwrap();
+    let is_emoji_presentation_ref = is_emoji_presentation_data.as_borrowed();
+
     fn compose(
         position: usize,
         container: &mut String,
         emoji_list: &mut Vec<(&'static Emoji, Range<usize>)>,
     ) {
-        if let Some(emoji) = lookup_by_glyph::lookup(container) {
+        if let Some(emoji) = lookup_emoji(container) {
             emoji_list.push((emoji, position..(position + container.len())));
         }
         container.clear();
@@ -59,7 +72,7 @@ fn find_emoji_ranges(
     let mut previous_emoji = false;
     let mut last_begin = 0usize;
     for (index, char) in content.as_ref().char_indices() {
-        let is_emoji_presentation = unic_emoji_char::is_emoji_presentation(char);
+        let is_emoji_presentation = is_emoji_presentation_ref.contains(char);
         let is_ascii = char.is_ascii();
         // Shortcut to ignore ascii characters which don't have a unicode presentation
         // for a good speed boost
@@ -71,10 +84,11 @@ fn find_emoji_ranges(
             }
             continue;
         }
-        let is_emoji = unic_emoji_char::is_emoji(char);
-        let is_emoji_component = unic_emoji_char::is_emoji_component(char);
-        let is_emoji_modifier_base = unic_emoji_char::is_emoji_modifier_base(char);
-        let is_emoji_modifier = unic_emoji_char::is_emoji_modifier(char);
+        // let is_emoji = unic_emoji_char::is_emoji(char);
+        let is_emoji = is_emoji_ref.contains(char);
+        let is_emoji_component = is_emoji_component_ref.contains(char);
+        let is_emoji_modifier_base = is_emoji_modifier_base_ref.contains(char);
+        let is_emoji_modifier = is_emoji_modifier_ref.contains(char);
         let is_emoji_variant = variation_selectors.contains(&char);
         if !previous_zwj {
             let is_empty = container.is_empty();
@@ -123,8 +137,8 @@ mod tests {
         let content = "Test 😘❤️! 😻💓 👨‍👩‍👦  kk 👨‍👩‍👧‍👦";
         let emojis = find_emoji(content);
         assert_eq!(emojis.len(), 6);
-        assert_eq!(emojis[0].name, "face blowing a kiss");
-        assert_eq!(emojis[5].name, "family: man, woman, girl, boy");
+        assert_eq!(emojis[0].name(), "face blowing a kiss");
+        assert_eq!(emojis[5].name(), "family: man, woman, girl, boy");
     }
 
     #[test]
@@ -139,7 +153,7 @@ mod tests {
         let content = "🇦🇩 🇪🇸Test ❤️ 😘 😻💓 👨‍👩‍👦  kk 👨‍👩‍👧‍👦";
         let emojis = find_emoji_ranges(content);
         for (emoji, range) in emojis {
-            assert_eq!(&content[range.clone()], emoji.glyph);
+            assert_eq!(&content[range.clone()], emoji.as_str());
         }
     }
 
@@ -148,6 +162,13 @@ mod tests {
         let content = "❤️";
         let emojis = find_emoji(content);
         assert_eq!(emojis.len(), 1);
-        assert_eq!(emojis[0].glyph, content);
+        assert_eq!(emojis[0].as_str(), content);
+    }
+
+    #[test]
+    fn test_new_emojis() {
+        let content = "🫘 🫶 🫡 🫠";
+        let emojis = find_emoji(content);
+        assert_eq!(emojis.len(), 4);
     }
 }
